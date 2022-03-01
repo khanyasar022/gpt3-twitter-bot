bed41e7935db61db8d823086aea9a77800e98faf
khanyasar022 <38328077+khanyasar022@users.noreply.github.com>
2022-03-02 00:09:30 +0530

    handling the twitter api and added openai

--- functions/index.js
+++ functions/index.js
@@ -5,13 +5,24 @@ admin.initializeApp()
 //DB reference
 const dbRef = admin.firestore().doc('tokens/env')
 
+
+//twitter api init
 const TwitterApi = require('twitter-api-v2').default;
 const twitterClient = new TwitterApi({
   clientId: 'process.env.CLIENT_ID',
   clientSecret: 'process.env.CLIENT_SECRET',
 })
 
-const callbackUrl = 'http://localhost:5000/gpt3-twitter-bot/us-central1/callback'
+const callbackURL = 'http://localhost:5000/gpt3-twitter-bot/us-central1/callback'
+
+
+//openai api init
+const { Configuration, OpenAIApi } = require('openai');
+const configuration = new Configuration({
+  organization: 'process.env.ORGANIZATION',
+  apiKey: 'process.env.API_SECRET',
+});
+const openai = new OpenAIApi(configuration);
 
 // auth url
 exports.auth = functions.https.onRequest((req, res) => {
@@ -52,5 +63,26 @@ exports.callback = functions.https.onRequest((req, res) => {
   res.send(data)
 })
 
-exports.tweet = functions.https.onRequest((req, res) => {})
+exports.tweet = functions.https.onRequest((req, res) => {
+    const { refreshToken } = (await dbRef.get()).data()
+
+  const {
+    client: refreshedClient,
+    accessToken,
+    refreshToken: newRefreshToken,
+  } = await twitterClient.refreshOAuth2Token(refreshToken)
+
+  await dbRef.set({ accessToken, refreshToken: newRefreshToken })
+
+  const nextTweet = await openai.createCompletion('text-davinci-001', {
+    prompt: 'random tweet for #techtwitter',
+    max_tokens: 64,
+  })
+
+  const { data } = await refreshedClient.v2.tweet(
+    nextTweet.data.choices[0].text
+  )
+
+  res.send(data)
+})
  
