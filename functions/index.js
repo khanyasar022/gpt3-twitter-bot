64af834c5816b3ea09cfd9dc03ec31e8c85787ec
khanyasar022 <38328077+khanyasar022@users.noreply.github.com>
2022-03-01 23:49:26 +0530

    basic setup complete with callback url and client keys

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,20 @@
-const functions = require("firebase-functions");
+const functions = require("firebase-functions")
+const admin = require('firebase-admin')
+admin.initializeApp()
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+//DB reference
+const dbRef = admin.firestore().doc('tokens/env')
+
+const TwitterApi = require('twitter-api-v2').default;
+const twitterClient = new TwitterApi({
+  clientId: 'process.env.CLIENT_ID',
+  clientSecret: 'process.env.CLIENT_SECRET',
+})
+
+const callbackUrl = 'http://localhost:5000/gpt3-twitter-bot/us-central1/callback'
+
+exports.auth = functions.https.onRequest((req, res) => {})
+
+exports.callback = functions.https.onRequest((req, res) => {})
+
+exports.tweet = functions.https.onRequest((req, res) => {})
